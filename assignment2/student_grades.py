3ca55324ec22aa5add3a4923ac3a2679a82411d2
Dharmesh Ajudiya <dharmeshajudiya@yahoo.in>
2025-10-13 17:59:28 +0530

    Update student_grades.py

--- assignment2/student_grades.py
+++ assignment2/student_grades.py
@@ -1,28 +1,40 @@
-students = {
-    "John": 'A',
-    "Jane": 'B',
-    "Jack": 'C',
-    "Jill": 'D',
-    "Joe": 'F'
-}
+students = {}
 
-for student, grade in students.items():
-    print(f"{student}: {grade}")
+is_running = True
 
-print("Adding a new student")
-name = input("Enter student name: ")
-grade = input("Enter student grade: ")
-students[name] = grade
+while is_running:
+    print("--------------------")
+    print("1. Add Student")
+    print("2. Update Grade")
+    print("3. View Students")
+    print("4 Exit")
+    print("--------------------")
 
-print("Updated student list:")
-for student, grade in students.items():
-    print(f"{student}: {grade}")
-
-print("Update a student's grade")
-name = input("Enter student name to update: ")
-grade = input("Enter new grade: ")
-students[name] = grade
-
-print("Final student list:")
-for student, grade in students.items(): 
-    print(f"{student}: {grade}")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        name = input("Enter student name: ")
+        grade = input("Enter student grade: ")
+        students[name] = grade
+        print(f"Student {name} added with grade {grade}")
+        print("--------------------")
+    
+    if choice == 2:
+        name = input("Enter student name to update: ")
+        if name in students:
+            grade = input("Enter new grade: ")
+            students[name] = grade
+            print(f"Student {name} updated with new grade {grade}")
+            print("--------------------")
+        else:
+            print(f"Student {name} not found")
+            print("--------------------")
+    
+    if choice == 3:
+        print("Student Grades:")
+        for name, grade in students.items():
+            print(f"{name}: {grade}")   
+        print("--------------------")
+    if choice == 4:
+        is_running = False
+        print("Exiting...")
+        print("--------------------")
